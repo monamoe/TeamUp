73ae3eee6298599922394c2fc6581b87231f6597
Riley Gray <46827629+rileylgray@users.noreply.github.com>
2021-03-22 09:03:39 -0400

    Added to profile page and binding in build gradle

--- app/src/main/java/app/helloteam/sportsbuddyapp/ProfilePage.kt
+++ app/src/main/java/app/helloteam/sportsbuddyapp/ProfilePage.kt
@@ -4,14 +4,24 @@ import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AlertDialog
+import app.helloteam.sportsbuddyapp.databinding.ActivityProfilePageBinding
+import com.parse.ParseUser
 
 class ProfilePage : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfilePageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile_page)
+        binding = ActivityProfilePageBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+        binding.userNameEdit.text= ParseUser.getCurrentUser().username
+        binding.dateText.text=ParseUser.getCurrentUser().createdAt.toString()
+
     }
     fun afterLogout() {//method to go back to login screen after logout
         val intent = Intent(this, MainActivity::class.java)
