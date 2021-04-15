ea9356766e8a5d1c609f819a396a1dde33c5cd11
soussamu <soussamu@sheridancollege.ca>
2021-04-15 12:45:46 -0400

    Minor edits

--- app/src/main/java/app/helloteam/sportsbuddyapp/views/EditProfilePage.kt
+++ app/src/main/java/app/helloteam/sportsbuddyapp/views/EditProfilePage.kt
@@ -24,6 +24,7 @@ import java.io.File
 import java.io.FileNotFoundException
 import java.io.IOException
 import java.io.InputStream
+import kotlin.jvm.Throws
 
 
 class EditProfilePage : AppCompatActivity() {
