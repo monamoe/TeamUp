2a0b7f51789bf6b5ca28798932c12ce3b6da14ba
Riley Gray <grayril@sheridancollege.ca>
2021-09-13 15:26:48 -0400

    Made job

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,11 @@
 const functions = require("firebase-functions");
 
 // eslint-disable-next-line max-len
-exports.timerUpdate = functions.pubsub.schedule("* * * * *").onRun((context) => {
-  console.log("successful timer update");
-  return null;
-});
+exports.eventExpired = functions.pubsub
+  .schedule("0/5 * * * *")
+  .timeZone("Canada/Toronto")
+  .onRun((context) => {
+    db.collection(Events)
+      .document("endDate" < new Date())
+      .delete()
+  });
