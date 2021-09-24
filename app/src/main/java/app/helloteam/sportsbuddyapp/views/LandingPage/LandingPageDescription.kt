3b5e520871a7d29a37086ac0ebec339759e43d7d
monamoe <arkhan2013@hotmail.com>
2021-09-23 21:03:58 -0400

    Jetpack Compose

---
+++ app/src/main/java/app/helloteam/sportsbuddyapp/views/LandingPage/LandingPageDescription.kt
@@ -0,0 +1,9 @@
+package app.helloteam.sportsbuddyapp.views.LandingPage
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PlantDetailDescription() {
+    Text("Hello Compose")
+}
