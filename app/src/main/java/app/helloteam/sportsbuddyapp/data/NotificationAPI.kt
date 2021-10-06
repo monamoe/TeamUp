728590b6a9424db76649360fe67373bb046ed360
Riley Gray <grayril@sheridancollege.ca>
2021-10-06 10:01:21 -0400

    hid key

--- app/src/main/java/app/helloteam/sportsbuddyapp/data/NotificationAPI.kt
+++ app/src/main/java/app/helloteam/sportsbuddyapp/data/NotificationAPI.kt
@@ -1,6 +1,8 @@
 package app.helloteam.sportsbuddyapp.data
 
 import app.helloteam.sportsbuddyapp.R
+import android.content.res.Resources.getSystem
+import com.google.common.io.Resources
 import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.Body
@@ -8,8 +10,7 @@ import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface NotificationAPI {
-
-    @Headers("Authorization: key=AAAAw5w1h4Q:APA91bF6U2DSgs3F2jBBrEybDT6TBirRXaA1W2XVSM8i_hLUOmDQRpnUG9hmtvjI7LelKcvnPHZmemkLz68W5RIANchB_0jgmJ1XilgNGyZouC-x0U505r6AC2Il0JZSOkX9kQcoHFSD", "Content-Type:application/json") // hide key
+    @Headers("Authorization: key=${Constants.server_key}", "Content-Type:application/json") // hide key
     @POST("fcm/send")
     suspend fun postNotification( //creates post request to send notif via firebase server
         @Body notification: PushNotification
